6d9adcc37f5a139096e804bf2a9ce54372f9c9a8
[xeroxzen] <[andilembele020@gmail.com]>
2019-08-05 11:25:23 +0200

    Organise your life

---
+++ daily_update.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 13 11:32:28 2019
+
+@author: Andile XeroxZen
+"""
+import time
+
+filename = 'daily_update.txt'
+day_entry = input("What is the day's update? __ ")
+today = time.ctime()
+
+msg = day_entry + ' ...Record of: {0}' .format(today)
+
+with open(filename, 'a') as f:
+    f.write(msg + '\n\n')
