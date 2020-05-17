3e156f8f8078022876e36b0b9370579f74848034
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-05-18 00:56:51 +0200

    How to use textwrap

--- daily_update.py
+++ daily_update.py
@@ -5,12 +5,15 @@ Created on Thu Jun 13 11:32:28 2019
 @author: Andile XeroxZen
 """
 import time
+import textwrap
 
 filename = 'daily_update.txt'
 day_entry = input("What is the day's update? __ ")
 today = time.ctime()
 
-msg = day_entry + ' ...Record of: {0}' .format(today)
+msg = day_entry + '\n Record of: {0}' .format(today)
+# message = textwrap.wrap(msg, width=100)
+# final_msg = list(message)
 
 with open(filename, 'a') as f:
     f.write(msg + '\n\n')
